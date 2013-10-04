03a6343aadfa6b36b60092c867e40032ea166ce3
Junior Oliveira <aoliveira@levelupgames.com.br>
2013-10-04 15:43:02 -0300

    Pacotes do NuGet para Library.Net e Library.Net.NHibernate

--- src/Library.Net.Data.NHibernate/Properties/AssemblyInfo.cs
+++ src/Library.Net.Data.NHibernate/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Library.Net.Data.NHibernate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library.Net.Data.NHibernate")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Library.Net.Data.NHibernate")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright © Junior Oliveira 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- src/Library.Net/Properties/AssemblyInfo.cs
+++ src/Library.Net/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Library.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library.Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Library.Net")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright © Junior Oliveira 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
