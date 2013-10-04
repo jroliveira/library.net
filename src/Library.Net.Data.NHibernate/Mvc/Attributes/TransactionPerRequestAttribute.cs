38606229041d1d954dbf5f59f0099b5dd44b5e70
Junior Oliveira <aoliveira@levelupgames.com.br>
2013-10-04 13:52:35 -0300

    Implementacoes para NHibernate

---
+++ src/Library.Net.Data.NHibernate/Mvc/Attributes/TransactionPerRequestAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Web.Mvc;
+using Library.Net.IoC;
+using NHibernate;
+
+namespace Library.Net.Data.NHibernate.Mvc.Attributes {
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TransactionPerRequestAttribute : ActionFilterAttribute {
+
+        private readonly IsolationLevel _isolationLevel;
+        private ISession _session;
+        private ITransaction _transaction;
+
+        public TransactionPerRequestAttribute(IsolationLevel isolationLevel = IsolationLevel.ReadUncommitted) {
+            _isolationLevel = isolationLevel;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            _session = DependencyContainerHelper.Get<ISession>();
+            _transaction = _session.BeginTransaction(_isolationLevel);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext) {
+            if (filterContext.Exception == null)
+                _transaction.Commit();
+            else
+                _transaction.Rollback();
+        }
+    }
+}
