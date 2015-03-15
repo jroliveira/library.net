9377ac780d0fe10d2d0588b3e40ece0f71647cca
Junior Oliveira <junolive@gmail.com>
2015-03-15 14:46:46 -0300

    Simplificando o model base

--- src/Library.Net.Mvc/Models/DefaultViewModel.cs
+++ src/Library.Net.Mvc/Models/DefaultViewModel.cs
@@ -1,15 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-
-namespace Library.Net.Mvc.Models {
+﻿namespace Library.Net.Mvc.Models {
 
     public abstract class DefaultViewModel {
 
-        protected abstract string MenuSelected { get; }
-        
-        public string IsActive(string menuItem) {
-            return MenuSelected == menuItem ? "active" : "";
-        }
+        public virtual string MenuSelected { get { return ""; } }
     }
 }
